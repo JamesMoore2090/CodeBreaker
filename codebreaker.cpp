47a7bd125ca550b1862704aee2394d6a9b1b747f
James Moore <jmoore7@rosemary.umw.edu>
2014-09-15 22:04:20 -0400

    A lot of work on codebreaker

--- codebreaker.cpp
+++ codebreaker.cpp
@@ -9,14 +9,20 @@ Dr. Davies
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <cmath>
+#include <cstring>
+#include <string>
 
 using namespace std;
+bool checkWords(string);
+
 
 int main(int argc, char* argv[]){
 string input;
-string fileChange;
+//string fileChange;
 int length;
 int count = 0;
+string dictionary[20] ={ "A", "THE", "BE", "TO", "OF", "AND", "THAT", "IN", "I", "IT", "FOR" , "NOT", "ON", "WITH", "HE", "AS", "YOU", "DO", "AT"};
 	if(argc!=2){
 		cout << "Usage: codebreaker [codebreakerTemplateFile]." << endl;
 		return 1;
@@ -28,18 +34,93 @@ int count = 0;
 		return 2;
 	}// end if
 	getline(file, input);
-	cout << input << endl;
 	length=input.length();
-	for(count= 0; count < length; count++){
-		for(int i=0; i < 1; i++){
-			if(input[count] == 'Z'){
-				input[count] = ' ';
+cout << input << " This is before it is messed with " << endl;
+	
+	for(int a=0; a<=26; a++){
+	if(isalpha(input[a])){	
+		for(int i =0; i<=length; i++){
+			cout << "This is what i is " << i << " " << input <<  endl;
+			if(input[i] ==' '){
+				input[i] = 'A';
+			cout << "This is where the space gets Changed to an A!!! " << input << endl;
 			}
-			else 
-				input[count]++;
-			
+			if(input[i] == 'Z'){
+				input[i] = 31;
+			}
+			input[i]++;
 		}
 	}
-	cout << "this is the new code " << input << endl;
+	if(checkWords(input) == true){
+		cout << input << endl;
+	}
+	cout << input << endl;
+	}	
+	
 return 0;
 }// end main
+
+//function to check the letters
+bool checkWords(string cript){
+int g = 0;
+if(g <=2){
+	if(cript== " A "){
+		g=g+1;
+	}
+	if(cript == " THE "){
+		g=g+1;
+	}
+	if(cript == " BE "){
+		g=g+1;
+	}
+	if(cript == " TO "){
+		g=g+1;
+	}
+	if(cript == " OF "){
+		g=g+1;
+	}
+	if(cript == " AND "){
+		g=g+1;
+	}
+	if(cript == " IN "){
+		g=g+1;
+	}
+	if(cript == " THAT "){
+		g=g+1;
+	}
+	if(cript == " I "){
+		g=g+1;
+	}
+	if(cript == " IT "){
+		g=g+1;
+	}
+	if(cript == " FOR "){
+		g=g+1;
+	}
+	if(cript == " NOT "){
+		g=g+1;
+	}
+	if(cript == " ON "){
+		g=g+1;
+	}
+	if(cript == " WITH "){
+		g=g+1;
+	}
+	if(cript == " HE "){
+		g=g+1;
+	}
+	if(cript == " AS "){
+		g=g+1;
+	}
+	if(cript == " YOU "){
+		g=g+1;
+	}
+	if(cript == " DO "){
+		g=g+1;
+	}
+	if(cript == " AT "){
+		g=g+1;
+	}
+}
+else return false;
+}
