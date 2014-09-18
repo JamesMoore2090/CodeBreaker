824688301fbea5c43f738f6ef8267e8edcd51a13
James Moore <jmoore7@rosemary.umw.edu>
2014-09-17 20:23:58 -0400

    This is the finished code!!!!

--- codebreaker.cpp
+++ codebreaker.cpp
@@ -14,12 +14,8 @@ Dr. Davies
 #include <string>
 
 using namespace std;
-bool checkWords(string, int);
-
-
 int main(int argc, char* argv[]){
 string input;
-//string fileChange;
 int length;
 	if(argc!=2){
 		cout << "Usage: codebreaker [codebreakerTemplateFile]." << endl;
@@ -33,8 +29,15 @@ int length;
 	}// end if
 	getline(file, input); // gets the string
 	length=input.length(); // gets the length of the string
-	for(int a=0; a<=26; a++){
-		for(int i =0; i<=length; i++){
+	for(int a=0; a<=26; a++){ // THis cycles through the 26 letters of the Aplhabet 
+	if(input[a] > 65 &&  input[a] < 90){ // keeps it in the aplhabet
+		for(int i =0; i<=length; i++){ // this cycles through the cipher
+			if(input[i] == 39){
+				input[i] = 38;
+			} // end if
+			if(input[i]== '.'){
+				input[i] = 45;
+			} // end if
 			if(input[i] ==' '){
 				input[i] = 64;
 			} // end if
@@ -43,35 +46,73 @@ int length;
 			} // end if
 			input[i]++;
 		} // end for
-	if(checkWords(input, length) == true){
+	int g =0;
+	if(input.find(" THE ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" BE ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" TO ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" OF ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" AND ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" A ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" IN ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" THAT ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" HAVE ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" I ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" IT ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" FOR ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" NOT ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" ON ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" WITH ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" HE ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" AS ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" YOU ")==-1){
+		g +=1;
+	}// end if
+	if(input.find(" DO ")==-1){
+	 	g += 1;
+	} // end if
+	if(input.find(" AT ")==-1){
+		g +=1;
+	}// end if
+//cout << g << "This is g" << endl;
+	if(g < 18){
 		cout << input << endl;
-	}// end for
-//	cout << input << endl;
-//	if(checkWords(input) == true){
-//		cout << input << endl;	
-	}
-//	else cout << "Your file has not been decrypted." << endl;
-		
-	
+	} // end if	
+	} // end for	
+	}//end if
+
 return 0;
 }// end main
-
-//function to check the letters
-bool checkWords(string cript, int len){
-	int count = 0;
-	string dictionary[20] = { "THE", "BE", "TO", "OF", "AND", "A", "IN", "THAT", "I", "IT", "FOR", "NOT", "ON", "WITH", "HE", "AS", "YOU", "DO", "AT"};
-
-	for(int d = 0; d <= 20; d++){
-		for(int l = 0; l <=len; l++){
-	//		for(int n = 0; n <=len; n++){
-				dictionary[d]==cript[l];
-				count++;
-	//		}
-			}	
-		}
-	
-	if(count >=2){
-		return true;
-	}
-	else return false;
-}
